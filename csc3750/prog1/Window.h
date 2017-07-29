ecda1c799141611c7d758402825c5884a3aa6d4c
dark777 <dark777@users.noreply.github.com>
2017-07-29 09:58:08 -0300

    Update Window.h
    Main.cpp: In function ‘int main(int, char**)’:
    Main.cpp:5:69: warning: ISO C++ forbids converting a string constant to ‘char*’ [-Wwrite-strings]
        Window window(argc, argv, 800, 600, 450, 250, "Computer Graphics");

--- csc3750/prog1/Window.h
+++ csc3750/prog1/Window.h
@@ -7,7 +7,7 @@ class Window
 {
 
    public:
-      Window(int argc, char** argv, int width, int height, int xLoc, int yLoc, char* title);
+      Window(int argc, char** argv, int width, int height, int xLoc, int yLoc, const char* title);
       ~Window();
       void show();
       static void reshape(int w,int h);
